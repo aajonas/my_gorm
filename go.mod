506da834a7d4ea0598fe31175335e5f0278e3e77
aajons <87728875@qq.com>
2022-03-05 16:41:38 +0800

    test gorm insert

--- go.mod
+++ go.mod
@@ -1,1 +1,8 @@
-module "github.com/aajonas/my_gorm"
+module github.com/aajonas/my_gorm
+
+go 1.14
+
+require (
+	gorm.io/driver/mysql v1.3.2
+	gorm.io/gorm v1.23.2
+)
--- main.go
+++ main.go
@@ -1,5 +1,50 @@
 package main
 
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID int
+	Phone string
+	Gender int8
+}
+
+func (User)TableName()string  {
+	return "hr_user"
+}
+
+//单插入
+func Insert(db *gorm.DB){
+	user := User{Phone: "嗷嗷嗷哦",Gender: 2}
+	db.Create(&user)
+	fmt.Println(user.ID)
+}
+
+//批量插入
+func Inserts(db *gorm.DB)  {
+	users := []User{{Phone: "嗷嗷嗷哦1",Gender: 2},{Phone: "嗷嗷嗷哦2",Gender: 2}}
+	db.Create(&users)
+	for _,v := range users {
+		fmt.Println(v.ID)
+	}
+}
+
 func main()  {
-	
+	dsn := "root:1234@tcp(127.0.0.1:3306)/hongren2?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err !=nil {
+		panic(err)
+	}
+
+	//单插入
+	//Insert(db)
+	//批量插入
+	//Inserts(db)
+	user := User{}
+	db.First(&user)
+	fmt.Printf("%#v",user)
+
 }
